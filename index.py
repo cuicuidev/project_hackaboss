4758491a7af77a3d757811f6902e6336889a0e10
Dmitry Ryzhenkov <dmitryryzhenkov.dev@gmail.com>
2023-08-14 12:02:31 -0400

    added basic file structure: index, routes and config files

--- index.py
+++ index.py
@@ -1,7 +1,18 @@
 import streamlit as st
 
+from config import PAGE_CONFIG
+from route_paths import routepaths
+
 def main():
-    pass
+
+    st.set_page_config(**PAGE_CONFIG)
+
+    try:
+        route_name = st.sidebar.selectbox(label = "Menu", options = list(routepaths.keys()), index = 0)
+
+        routepaths[route_name]()
+    except KeyError as e:
+        st.error(f'Ha acurrido un error seleccionando la página. La clave `{e}` no existe en `routepaths`')
 
 if __name__ == '__main__':
     main()
