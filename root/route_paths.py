ea908925c71f261de0be93564b22b10c87dc1a07
Dmitry Ryzhenkov <dmitryryzhenkov.dev@gmail.com>
2023-08-16 12:16:15 -0400

    added eda.py

--- root/route_paths.py
+++ root/route_paths.py
@@ -1,7 +1,9 @@
 from routes.main import mainRoute
 from routes.about import aboutRoute
+from routes.eda import edaRoute
 
 routepaths = {'Main' : mainRoute, #Landing Page
+              'EDA' : edaRoute,
               'About' : aboutRoute,
               # Añadir mas rutas
               }
