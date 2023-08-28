d11efb96111b0631cf7d6d7be7fc089407460f97
Dmitry Ryzhenkov <dmitryryzhenkov.dev@gmail.com>
2023-08-28 19:32:14 +0200

    increase section2

--- routes/main.py
+++ routes/main.py
@@ -2,7 +2,7 @@ from components.section import Section
 
 header = Section(text = '##### _Haciendo uso de redes neurales convolucionales para clasificar imagenes de vehículos en base a sus marcas_', title = 'Clasificación de marcas de vehículos')
 section1 = Section(text = 'Section 1 '*100, header = 'Definición del problema', media = lambda x: x.image('assets/Tensorflow_logo.png', width = 500))
-section2 = Section(text = 'Section 2 '*100, header = 'Dataset')
+section2 = Section(text = 'Section 2 '*200, header = 'Dataset')
 
 def mainRoute():
 
