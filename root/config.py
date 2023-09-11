bc9501cd04976f61f227831b60601ad821a8da7f
Dmitry Ryzhenkov <dmitryryzhenkov.dev@gmail.com>
2023-09-12 01:40:00 +0200

    maybe a good architecture for grayscale

--- root/config.py
+++
@@ -1,4 +0,0 @@
-PAGE_CONFIG = {"page_title"             : "Proyecto 3 Hackaboss",
-                "page_icon"             : ":seedling:",
-                # "layout"                : "wide",
-                "initial_sidebar_state" : "expanded"}
--- routes/workflow.py
+++
@@ -1,2 +0,0 @@
-def workflowRoute():
-    pass
